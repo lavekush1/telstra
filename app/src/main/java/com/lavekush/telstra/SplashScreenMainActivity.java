9025b9b7121f9d0a79881d1d7add7699b445264f
Lavekush Agrawal <lagrawal.nri@gmail.com>
2017-11-25 19:32:07 +0530

    Added splash screen & basic app resource(Like dimen/string/color)

---
+++ app/src/main/java/com/lavekush/telstra/SplashScreenMainActivity.java
@@ -0,0 +1,38 @@
+package com.lavekush.telstra;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenMainActivity extends AppCompatActivity {
+
+    // Splash screen timeout delay
+    private static int SPLASH_TIME_OUT_DELAY = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_main);
+
+        //Handler for background task management
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer.
+             * This can be replace by some configuration api call delay
+             */
+
+            @Override
+            public void run() {
+
+                // This method will be executed once the timer is over
+                Intent intentHomeActivity = new Intent(SplashScreenMainActivity.this, HomeActivity.class);
+                startActivity(intentHomeActivity);
+
+                // close current this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT_DELAY);
+    }
+}
