c84248824a62c8d4899b9a25a57b8fc914a9114d
Lavekush Agrawal <lagrawal.nri@gmail.com>
2017-11-26 14:13:21 +0530

    Added app class in Manifest.xml

--- app/src/main/java/com/lavekush/telstra/TelstraApplication.java
+++ app/src/main/java/com/lavekush/telstra/TelstraApplication.java
@@ -2,6 +2,9 @@ package com.lavekush.telstra;
 
 import android.app.Application;
 
+/**
+ * Application instance for third party api installer like crashreporting , analytics (Not in use for now)
+ */
 public class TelstraApplication extends Application {
 
     @Override
